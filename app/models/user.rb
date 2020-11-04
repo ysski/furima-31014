9acc342cc89a02eea3781c89ccad41dc83cceb91
ysski <yutasasaki2216@gmail.com>
2020-11-04 20:55:49 +0900

    ユーザー機能管理の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  validates :password, presence: true, length: { minimum: 6 }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :hiragana_name_first
+    validates :hiragana_name_last
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
+    validates :katakana_name_first
+    validates :katakana_name_last
+  end
+  
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
