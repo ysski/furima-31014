fab7bc072625f3d4b1c658e3915cc9b0f26ba7bd
ysski <yutasasaki2216@gmail.com>
2020-11-10 18:04:14 +0900

    Update items_controller.rb
     before_action set_item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
   before_action :move_to_index2, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -12,15 +12,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
+  def edit   
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -54,6 +51,10 @@ class ItemsController < ApplicationController
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
   
 end
