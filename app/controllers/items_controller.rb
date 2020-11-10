f3ed268f8a67e768221f22dc78f9c5cf92a19ee6
ysski <yutasasaki2216@gmail.com>
2020-11-10 20:40:08 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
-  before_action :move_to_index2, except: [:create, :new, :index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index2, except: [:create, :new, :index, :show, :destroy]
   
   def index
     @items = Item.order("created_at DESC")
@@ -45,12 +44,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :shipping_charge_id, :ship_from_id, :days_to_ship_id, :price, :genre_id, :condition_id).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
 
   def move_to_index2
